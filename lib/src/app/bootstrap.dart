cb8efcfcc2c22307443c1830f38c4e649d8d2832
Trinh Quoc Khanh <124611634+khanhcy@users.noreply.github.com>
2026-04-06 17:49:03 +0700

    feat: add base backend, connect firebase

--- lib/src/app/bootstrap.dart
+++ lib/src/app/bootstrap.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:smartnutri/src/app/smartnutri_app.dart';
 import 'package:smartnutri/src/core/config/app_environment.dart';
+import 'package:smartnutri/src/core/firebase/firebase_options.dart';
 import 'package:smartnutri/src/core/services/auth_service.dart';
 import 'package:smartnutri/src/core/services/profile_service.dart';
 
 Future<void> bootstrap() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   final environment = AppEnvironment.fromDartDefines();
 
